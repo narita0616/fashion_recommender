50b808b0899c4ae1bd1cb0b5e583cb4195158587
narita_rikuto <naritari0616@icloud.com>
2023-04-29 02:05:35 +0900

    プロジェクト作成コミット

---
+++ src/main/java/com/example/fashion_recomendar/FashionRecomendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashion_recomendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionRecomendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionRecomendarApplication.class, args);
+	}
+
+}
